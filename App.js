9e0004d50e01cef115e10fd909ac43843df0cdbd
Alok041 <102008660+Alok041@users.noreply.github.com>
2023-10-25 18:21:49 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTodo = () => {
+    if (task) {
+      setTodos([...todos, { text: task, isComplete: false }]);
+      setTask('');
+    }
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const toggleComplete = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isComplete = !newTodos[index].isComplete;
+    setTodos(newTodos);
+  };
+
+  const completeAll = () => {
+    const updatedTodos = todos.map((todo) => ({ ...todo, isComplete: true }));
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <div>
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+        <button onClick={completeAll}>Complete All</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index} className={todo.isComplete ? "completed" : ""}>
+            <span onClick={() => toggleComplete(index)}>{todo.text}</span>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
